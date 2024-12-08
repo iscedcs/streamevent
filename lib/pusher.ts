59bd37ec1be1bbfad4b4ae085b8b0bb62fc829a4
rexedge <ap.oyeniran@gmail.com>
2024-12-08 13:13:34 +0100

    fix(pusher): :bug: changing from cluster env to string

--- lib/pusher.ts
+++ lib/pusher.ts
@@ -5,13 +5,13 @@ export const pusherServer = new PusherServer({
   appId: process.env.PUSHER_APP_ID!,
   key: process.env.NEXT_PUBLIC_PUSHER_KEY!,
   secret: process.env.PUSHER_SECRET!,
-  cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
+  cluster: "mt1",
   useTLS: true,
 });
 
 export const pusherClient = new PusherClient(
   process.env.NEXT_PUBLIC_PUSHER_KEY!,
   {
-    cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
+    cluster: "mt1",
   }
 );
